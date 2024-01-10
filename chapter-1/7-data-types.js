920c36d4cac35a6340f08a564666f3207a2cf931
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-10 22:19:33 +0530

    Create 7-data-types.js

---
+++ chapter-1/7-data-types.js
@@ -0,0 +1,18 @@
+var x = 100; // x is a number
+var y = 3.14; // y is a number
+var z = "John"; // z is a string
+var a = 'John'; // a is a string
+var b = true; // b is a boolean
+var c = false; // c is a boolean
+var d = null; // d is a null
+var e; // e is undefined
+var e_e = undefined; // e_e is undefined
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
