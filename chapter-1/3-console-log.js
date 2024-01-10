ecc1a57473d047903cdd058b485032296b22b0bf
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-10 20:32:46 +0530

    Update 3-console-log.js

--- chapter-1/3-console-log.js
+++ chapter-1/3-console-log.js
@@ -2,7 +2,7 @@ console.log("hello world");
 console.log("good evening!!!");
 /**
  * To run this code, open command prompt and type
- * node 3-var.js
+ * node 3-console-log.js
  * 
  * Please note that you need to be in the same directory
  * as this file
