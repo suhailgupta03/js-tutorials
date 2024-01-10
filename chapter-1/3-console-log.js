d1f9d172f6f40398bef255b6d45d77d493de1e52
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-10 20:31:25 +0530

    basics - 1

---
+++ chapter-1/3-console-log.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+console.log("good evening!!!");
+/**
+ * To run this code, open command prompt and type
+ * node 3-var.js
+ * 
+ * Please note that you need to be in the same directory
+ * as this file
+ */
