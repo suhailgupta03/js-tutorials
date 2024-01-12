125e5b8723f7fbc97fd1ce9ca5f68eb84b7b34ce
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-12 21:21:06 +0530

    Create 4-comparison-op.js

---
+++ chapter-2/4-comparison-op.js
@@ -0,0 +1,22 @@
+var x = 100;
+var y = 200;
+/**
+ * The comparison operators are used to compare two values.
+ * and return true or false depending on the result.
+ * Boolean : true or false
+ */
+console.log(x > y); // false
+console.log(x < y); // true
+console.log(x == y); // false
+console.log(3 == 3); // true
+console.log(4 > 4); // false
+console.log(4 >= 4); // true
+// >= is read as greater than or equal to
+console.log(100 >= 4); // true
+console.log(100 <= 4); // false
+console.log(4 <= 4); // true
+
+console.log(4 != 4); // false
+// != is read as not equal to
+// ! is read as not
+console.log(3 != 4); // true
