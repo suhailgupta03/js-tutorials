20a96fb4506391a7fa4732458abe9aac409b28e9
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-12 20:24:15 +0530

    Create 1-arithmetic-op.js

---
+++ chapter-2/1-arithmetic-op.js
@@ -0,0 +1,21 @@
+var x = 200; // x is a variable and 200 is a value
+var y = 100; // y is a variable and 100 is a value
+
+
+console.log(x + y); // + is a arithmetic operator 
+// that adds the value of x and y
+
+console.log(y - x); // - is a arithmetic operator
+// that subtracts the value of x from y
+
+console.log(x * y); // * is a arithmetic operator
+// It is represented using an asterisk
+// and it multiplies the value of x and y
+
+console.log(x / y); // / is a arithmetic operator
+// It gives out the quotient of x and y
+
+console.log(x % y); // % is a arithmetic operator
+// It is called the modulus operator
+// It gives out the remainder of x and y
+console.log(3%2); // 1
