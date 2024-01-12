38753774e32961e408feda361f5129012e1181c8
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-12 22:11:34 +0530

    Create 6-string-op.js

---
+++ chapter-2/6-string-op.js
@@ -0,0 +1,8 @@
+var firstName = "John";
+var lastName = "Doe";
+
+var fullName = firstName + " " +  lastName;
+/**
+ * Here we are using the + operator to concatenate two strings.
+ */
+console.log(fullName);
