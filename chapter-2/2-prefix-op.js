e6024264a0035c1a16438b8c1e318f33b379182f
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-12 20:41:08 +0530

    Create 2-prefix-op.js

---
+++ chapter-2/2-prefix-op.js
@@ -0,0 +1,21 @@
+var x = 100;
+console.log(x + 1); // + operator to add 2 numbers
+
+var y = x + 1; // 100 + 1
+// first assigning the value of x + 1 to y
+console.log(y);
+// and then printing the value of y
+
+var z = ++x;
+// ++x is a prefix operator
+// It is same as x = x + 1
+// We call it as prefix operator because
+// it is placed before the variable name
+// It is only used as a shorthand for x = x + 1
+console.log(z);
+
+
+var zz = --x;
+// --x is a prefix operator
+// It is same as x = x - 1
+console.log(zz);
