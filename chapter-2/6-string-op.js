2794c38f12fbfc4ab1a825c135ab5a90a38976d1
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-12 22:17:01 +0530

    Update 6-string-op.js

--- chapter-2/6-string-op.js
+++ chapter-2/6-string-op.js
@@ -1,8 +1,18 @@
 var firstName = "John";
 var lastName = "Doe";
 
-var fullName = firstName + " " +  lastName;
+var fullName = firstName + " " +  lastName + ".....";
 /**
  * Here we are using the + operator to concatenate two strings.
  */
 console.log(fullName);
+
+var x = 10;
+var fruitName = "Banana";
+
+console.log(x + fruitName);
+// x is converted to string and concatenated with fruitName
+console.log(fruitName + x);
+// x is converted to string and concatenated with fruitName
+console.log(false + "Apple");
+console.log(true + "Apple");
