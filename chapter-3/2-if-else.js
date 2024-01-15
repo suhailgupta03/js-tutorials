1498d6dd2ffd5a1ed3456862543ce8d429fda2f3
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-15 20:51:33 +0530

    Update 2-if-else.js

--- chapter-3/2-if-else.js
+++ chapter-3/2-if-else.js
@@ -1,20 +1,18 @@
 var x = 100;
-var y = 200;
-
-if(y > x) { // 200 > 100 --> TRUE
+var y = -1;
+/**
+ * Else always comes after if block
+ * It cannot come alone or before if block
+ */
+if (y > x) { // 200 > 100 --> TRUE
     console.log('y is greater than x');
     console.log("INSIDE IF BLOCK");
     console.log("HELLO WORLD!!");
+}else {
+    console.log("Inside else block");
+    console.log('y is less than x'); 
 }
 
-/**
- * Second example
- */
-
-var isSnowing = true;
-var haveTrekkingShoes = true;
-
-if (isSnowing && haveTrekkingShoes) {
-  console.log('We are going to trekking.');
-}
-
+console.log("..Done with if-else block");
+var yy = 3 + 2;
+console.log(yy);
