0d722680f58704932fdec6349c2b8c03742b7328
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-15 21:08:46 +0530

    Create 3-if-else.js

---
+++ chapter-3/3-if-else.js
@@ -0,0 +1,12 @@
+var x = 100;
+var y = 200;
+
+/**
+ * Curly braces are not mandatory if there is only one statement
+ * They are only mandatory, if there are more than one statements
+ * inside if or else block
+ */
+if (y > x) // 200 > 100 
+    console.log('y is greater than x');
+else
+    console.log('y is less than x');
