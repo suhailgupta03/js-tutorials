6f396dc92d2de489fe65dd13c5cb64238aac0a9d
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-15 20:29:15 +0530

    ch-3

---
+++ chapter-3/2-if-else.js
@@ -0,0 +1,20 @@
+var x = 100;
+var y = 200;
+
+if(y > x) { // 200 > 100 --> TRUE
+    console.log('y is greater than x');
+    console.log("INSIDE IF BLOCK");
+    console.log("HELLO WORLD!!");
+}
+
+/**
+ * Second example
+ */
+
+var isSnowing = true;
+var haveTrekkingShoes = true;
+
+if (isSnowing && haveTrekkingShoes) {
+  console.log('We are going to trekking.');
+}
+
