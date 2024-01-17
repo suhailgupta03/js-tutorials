c81458f8201bdbf65af67511a0f544f3822b52a8
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-17 20:46:01 +0530

    Create 2.for.js

---
+++ chapter-4/2.for.js
@@ -0,0 +1,9 @@
+for(var i = 0; i < 3; i++) {
+    console.log(i);
+    console.log("....")
+}
+
+// 1st step: i =0, i < 3 (TRUE) -> console.log(0) -> i = 1
+// 2nd step: i =1, i < 3 (TRUE) -> console.log(1) -> i = 2
+// 3rd step: i =2, i < 3 (TRUE) -> console.log(2) -> i = 3
+// 4th step: i =3, i < 3 (FALSE) -> exit the loop
