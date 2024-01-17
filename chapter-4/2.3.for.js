fdb217f08ea9a59f0b50848c2234d104b2608358
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-17 21:20:48 +0530

    Create 2.3.for.js

---
+++ chapter-4/2.3.for.js
@@ -0,0 +1,7 @@
+for(var i = 1; i <= 10; i++) {
+    console.log(
+        "9 x " + i + " = " + (9 * i)
+        // The above is just concatenation of strings
+    ) // 9 x 1 = 9
+      // 9 x 2 = 18
+}
