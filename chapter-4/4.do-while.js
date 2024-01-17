f15b93a43f3034b1f56b51f5b253339da60bca69
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-17 22:14:35 +0530

    Create 4.do-while.js

---
+++ chapter-4/4.do-while.js
@@ -0,0 +1,11 @@
+var x = 0;
+do { // body of the loop that will be executed at least once
+    // code block to be executed
+    console.log(x);
+    x++;
+}while(x <= 1); // condition
+
+/**
+ * do-while loop is used when you want to execute the code block 
+ * at least once,
+ */
