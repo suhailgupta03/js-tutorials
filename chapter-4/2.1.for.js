aef7b0568ce71bec7db7f517ffafc4f0f7f5511e
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-17 21:06:31 +0530

    Create 2.1.for.js

---
+++ chapter-4/2.1.for.js
@@ -0,0 +1,12 @@
+var i = 0; // Initialization
+for(;;) {
+    if(i < 3) { // Condition
+        console.log(i);
+        console.log("....")
+        i++; // Increment
+    }
+}
+
+// 1st step: i =0, i < 3 (TRUE) -> console.log(0) -> i = 1
+// 2nd step: i =1, i < 3 (TRUE) -> console.log(1) -> i = 2
+// 3rd step: i =2, i < 3 (TRUE) -> console.log(2) -> i = 3
