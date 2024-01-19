43950a169fe2299f346237755d131e8959c4a866
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-19 21:25:11 +0530

    Create 3.array-push-pop.js

---
+++ chapter-5/3.array-push-pop.js
@@ -0,0 +1,22 @@
+var studentNames = [
+    "Sami",
+    "Kamal",
+    "Jamal",
+    "Rahim",
+    "Karim"
+];
+
+console.log(studentNames);
+studentNames.push("Joe");
+
+console.log(studentNames);
+studentNames.push("John");
+
+console.log(studentNames);
+
+studentNames.pop(); // removes the last element
+console.log(studentNames);
+studentNames.pop(); // removes the last element
+console.log(studentNames);
+studentNames.pop(); // removes the last element
+console.log(studentNames);
