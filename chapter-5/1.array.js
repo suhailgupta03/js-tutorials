477d62d201f5d4b64a380df4db4c461768308c31
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-19 20:46:13 +0530

    Create 1.array.js

---
+++ chapter-5/1.array.js
@@ -0,0 +1,11 @@
+var ageOfStudents = [19, 25, 18, 1, 2, 99];
+console.log(ageOfStudents);
+
+// To print the first value inside array, we use index 0
+console.log(ageOfStudents[0]);
+// To print the second value inside array, we use index 1
+console.log(ageOfStudents[1]);
+// To print the last value inside array, we use index length - 1
+console.log(ageOfStudents[5])
+
+console.log(ageOfStudents[2]);
