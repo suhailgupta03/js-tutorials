ecfe5889e3df676eb0691df28817b18b11ecaecf
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-19 22:04:22 +0530

    Create 5.strings.js

---
+++ chapter-5/5.strings.js
@@ -0,0 +1,16 @@
+var name = "John";
+var name1 = 'John';
+var name2 = `John`;
+
+console.log(name, name1, name2)
+
+var sentence = name + " ... " + name1 + " ... " + name2;
+console.log(sentence)
+
+var sentence  = "Quick brown fox jumps over the lazy dog";
+console.log(sentence.length);
+
+var studentName = "John Doe";
+console.log(studentName[0]);
+console.log(studentName[1]);
+console.log(studentName[studentName.length - 1]);
