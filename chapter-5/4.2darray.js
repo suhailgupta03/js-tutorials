1e03ff4077e88d6a8fb9dd25d6525a6c1adf906f
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-19 21:53:32 +0530

    Create 4.2darray.js

---
+++ chapter-5/4.2darray.js
@@ -0,0 +1,11 @@
+var xyz = [
+    [1, 2, 3, 4], // 0th index
+    [5, 6, 7, 8], // 1st index
+    [9 ,10, 11, 12] // 2nd index
+]
+
+console.log(xyz);
+console.log(xyz[0]);
+console.log(xyz[1]);
+console.log(xyz[0][1]); // prints 2
+console.log(xyz[2][2]); // prints 11
