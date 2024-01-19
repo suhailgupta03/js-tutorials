d9daa74917258496f81cb844e51bbe361342b208
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-19 21:09:30 +0530

    Create 2.array.js

---
+++ chapter-5/2.array.js
@@ -0,0 +1,15 @@
+var ageOfStudents = [19, 25, 18, 1, 2, 99, 0, 88];
+/**
+ * In the following loop, we are iterating through the array
+ * We stop when we have reached the last value inside the array
+ */
+for(var i = 0; i < ageOfStudents.length; i++) {
+    console.log(ageOfStudents[i]);
+}
+
+// 1st iteration: i = 0; i < 8; console.log(ageOfStudents[0]); i = 1;
+// 2nd iteration: i = 1; i < 8; console.log(ageOfStudents[1]); i = 2;
+// 3rd iteration: i = 2; i < 8; console.log(ageOfStudents[2]); i = 3;
+// ..7th iteration: i = 7; i < 8; console.log(ageOfStudents[7]); i = 8;
+// 8th iteration: i = 8; i < 8; exit loop;
+
