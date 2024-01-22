644003ba5e4539c5af2b24bb5e74b8c52867cc34
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-22 21:54:49 +0530

    Create 6.object.js

---
+++ chapter-6/6.object.js
@@ -0,0 +1,9 @@
+var mobilePhone = {
+    brand: 'Samsung',
+    model: 'Galaxy Note 9',
+    color: 'Midnight Black',
+    price: 999,
+    isInsured: true
+};
+console.log(Object.keys(mobilePhone)); // ["brand", "model", "color", "price", "isInsured"]
+console.log(Object.values(mobilePhone)); // ["Samsung", "Galaxy Note 9", "Midnight Black", 999, true]
