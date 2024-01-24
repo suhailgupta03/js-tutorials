8f7d6e4cda76b8e2a7193e7b995d354685e68de2
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-24 21:05:52 +0530

    Create 2.fn.js

---
+++ chapter-7/2.fn.js
@@ -0,0 +1,14 @@
+/**
+ * Following is a function definition:
+ *  - that adds two numbers 10 and 20
+ *  - and prints the result
+ */
+function sum(x, y) {
+    var z = x + y;
+    console.log(z);
+}
+
+sum(2, 3);      // to call the function,we use the function name followed by parenthesis
+sum(10, 20);    // to call the function,we use the function name followed by parenthesis
+sum(99, 88);    // to call the function,we use the function name followed by parenthesis
+sum(99, 1)
