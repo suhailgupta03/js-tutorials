242d37299c5244dcf0526697ddbf58ccd86f9236
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-24 21:20:12 +0530

    Create 3.fn.js

---
+++ chapter-7/3.fn.js
@@ -0,0 +1,18 @@
+/**
+ * Following is a function definition:
+ *  - that adds two numbers
+ *  - has x and y as parameters
+ *  - and prints the result
+ */
+function sum(x, y) {
+    /**
+     * x and y are called parameters or arguments
+     */
+    var z = x + y;
+    return z;
+}
+
+// sum(1,2) is an expression that evaluates to 3
+// sum(1,2) => 3
+var result = sum(1,2);
+console.log(result);
