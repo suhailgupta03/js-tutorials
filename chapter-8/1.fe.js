12c64177c244d3d9a013512d2bdfe735d2a53943
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-25 20:14:40 +0530

    Create 1.fe.js

---
+++ chapter-8/1.fe.js
@@ -0,0 +1,12 @@
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+var x = function(a, b) {
+    // This is called the function expression
+    // because it is assigned to a variable
+    return a * b;
+}
+
+console.log(x); // will print the function definition
+console.log(x(2,3)); // will print the result of the function 2*3 = 6
