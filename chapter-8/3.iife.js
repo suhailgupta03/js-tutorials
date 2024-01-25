0975f93090cf08a29e1c524f583efc1e99b2a53e
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-25 20:44:23 +0530

    Create 3.iife.js

---
+++ chapter-8/3.iife.js
@@ -0,0 +1,23 @@
+// anonymous -> that does not have a name
+(function() {
+    console.log("hello");
+})();
+
+// iife -> immediately invoked function expression
+
+/**
+ * Breakdown of iife
+ */
+
+// 1. function expression
+// (function() {
+//     console.log("hello");
+// })
+
+// 2. invoke the function
+// by adding (); at the end of the function expression
+
+// 3. immediately invoked
+// (function() {
+//     console.log("hello");
+// })();
