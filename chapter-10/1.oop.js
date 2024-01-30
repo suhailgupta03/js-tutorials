af2e136ecba8dd6c1ae234c56121d8cabc2c49d2
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-30 20:41:59 +0530

    Create 1.oop.js

---
+++ chapter-10/1.oop.js
@@ -0,0 +1,43 @@
+    class Animal {
+        /**
+         * This will be called when the object is created
+         * using the new keyword. 
+         * 
+         * We call this as a "constructor" function
+         */
+        constructor(name, sc) {
+            /**
+             * this refers to the "CURRENT OBJECT"
+             * The object that is being created
+             */
+            this.myName = name
+            this.skinColor = sc;
+            /**
+             * Here myName and skinColor are regular object
+             * attributes. It is exactly similar when create 
+             * an object in the following way:
+             * var xyz = {
+             *    myName: "Lion",
+             *    skinColor: "Brown"
+             * }
+             */
+        }
+
+        eat() {
+            console.log(this.myName + " eats food");
+        }
+
+        sleep() {
+            console.log(this.myName + " sleeps");
+        }
+    }
+
+    const lion = new Animal("Lion", "Brown");
+    console.log(xyz);
+    lion.eat(); // we use the dot operator to access the attributes as well as the functions
+    lion.sleep();
+
+    const tiger = new Animal("Tiger", "White");
+    console.log(abc);
+    tiger.eat();
+    tiger.sleep();
