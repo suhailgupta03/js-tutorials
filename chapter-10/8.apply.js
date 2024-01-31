09c2603dfc8273d6f125b57fb687fd13a13cf279
Suhail Gupta <suhailgupta03@gmail.com>
2024-01-31 21:17:30 +0530

    Update 8.apply.js

--- chapter-10/8.apply.js
+++ chapter-10/8.apply.js
@@ -1,12 +1,14 @@
 function updateCoordinates(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
+    this.x = x; // setting key as 'x' and value as x
+    this.y = y; // setting key as 'y' and value as y
+    this.z = z; // setting key as 'z' and value as z
+
+    // Here this means coordinates object
 
     console.log(this);
 }
 
-var coordinates = { x: 0, y: 0, z: 0};
+var coordinates = {name: "manish"}; // inital values
 updateCoordinates.call(coordinates, 2, 2, 2);
 updateCoordinates.apply(coordinates, [2, 2, 2]);
 
