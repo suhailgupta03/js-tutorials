25ac6a5553bd71b2a18e9d2a39e992de44762181
Suhail Gupta <suhailgupta03@gmail.com>
2024-02-05 21:23:00 +0530

    Update 5.spread.js

--- chapter-12/5.spread.js
+++ chapter-12/5.spread.js
@@ -3,5 +3,19 @@ var studentList = ["Manoj", "Raj", "Rahul", "Rohit", "Rajesh"];
 // var secondStudent = studentList[1];
 // var thirdStudent = studentList[2];
 // var [firstStudent, secondStudent, thirdStudent] = studentList;
-var [x, y, ...remainingStudents] =  studentList;
+var [x, y, ...remainingStudents] =  studentList; // Array destructuring
 console.log(x, y, remainingStudents);
+
+var student = {
+    name: "Manoj",
+    age: 25,
+    address: "Kathmandu",
+    email: "abc@def.com",
+    phone: "1234567890"
+}
+
+// var name = student.name;
+// var age = student.age;
+// var address = student.address;
+var {age, address, name, ...rest} = student; // Object destructuring
+console.log(name, age, address, rest);
