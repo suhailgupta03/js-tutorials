e6e71dcd219367625657ae9e809fc1d8167e0b5c
Suhail Gupta <suhailgupta03@gmail.com>
2024-02-05 20:17:59 +0530

    Create 1.arrow.js

---
+++ chapter-12/1.arrow.js
@@ -0,0 +1,20 @@
+// var sumFnRegular = function (a, b) {
+//     console.log("Adding two numbers...");
+//     return a + b;
+// }
+
+// var result = sumFnRegular(1, 99);
+// console.log(result);
+// ----------------------------
+/**
+ * Following is the arrow function syntax
+ * (param1, param2, ..., paramN) => { statements }
+ * 
+ */
+const sumFn = (a, b) => {
+    console.log("Adding two numbers...");
+    return a + b;
+}
+
+var result1 = sumFn(1, 99);
+console.log(result1);
