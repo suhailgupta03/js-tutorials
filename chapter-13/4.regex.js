72a3a685f9596aec062d23f4ea5cffdf28937e66
Suhail Gupta <suhailgupta03@gmail.com>
2024-02-06 21:32:36 +0530

    Update 4.regex.js

--- chapter-13/4.regex.js
+++ chapter-13/4.regex.js
@@ -1,9 +1,18 @@
-var string = "#1234567abcdef123jklopmnwerabcdefghipklabcmno/98+-@#^*?abc";
-
-const pattern = /abc/g;
-var result;
-
-// In javascript 'null' is a FALSY value
+var string = "dfhsyabc;awy8xqry a-z (*&#^@%@#^&(@#*@_#)R&(R024r2r9";
+var pattern = /abc/;
+// /abc/.exec withouyt t
 while(result = pattern.exec(string)) {
     console.log(result);
 }
+
+// var string = "dfhsyabc;awy8xqry a-z (*&#^@%@#^&(@#*@_#)R&(R024r2r9";
+// var pattern = /abc/;
+// // /abc/.exec without the global flag will always return the first match
+// // no matter how many times you call it 
+
+// // but when you add a global flag it will 
+// // return all the matches and once it has returned all the matches
+// // it will simply return null
+// while(result = pattern.exec(string)) {
+//     console.log(result);
+// }
