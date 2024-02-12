a8f62c33371fdf43afafb935d18e7056951baa34
Suhail Gupta <suhailgupta03@gmail.com>
2024-02-12 20:30:07 +0530

    Create 8.regex.js

---
+++ chapter-13/8.regex.js
@@ -0,0 +1,12 @@
+/**
+ * [a-z0-9]
+ * The above regular expression means one of a-z or one of 0-9
+ */
+const pattern = /[a-z0-9\_]+@gmail\.com/g;
+const email = "manas@gmail.com";
+const email1 = "shreya99@hotmail.com"
+
+const result = email.match(pattern);
+console.log(result);
+const result1 = email1.match(pattern);
+console.log(result1);
