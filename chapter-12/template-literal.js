024db9143978be91412ee5df676c6ec41bc344aa
Suhail Gupta <suhailgupta03@gmail.com>
2024-02-12 21:43:02 +0530

    Create template-literal.js

---
+++ chapter-12/template-literal.js
@@ -0,0 +1,10 @@
+var x = "apple";
+var y = "banana";
+
+var newString = `I have a ${x} and a ${y} tree.`
+// ${} is a template literal
+// It is used to insert a variable into a string
+console.log(newString)
+
+console.log(`${x} .... ${y}`)
+console.log(x + " .... " + y);
