ee0b550ec1d11eb028af82fe2729923736fb3fe9
Suhail Gupta <suhailgupta03@gmail.com>
2024-03-08 21:39:17 +0530

    Update App.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,37 +1,12 @@
-import {
-  ToDoText
-} from "./todo-text"
+import {UsernameInput} from "./UsernameInput"
 
-function loadTodoListFromServer() {
-  return [
-    "Have breakfast at 9:00 AM",
-    "Leave for office by 10:00 AM",
-    "Have lunch at 2:00 PM",
-    "Start back by 6:00 PM",
-    "Have dinner by 9:00 PM",
-    "Sleep by 11:00 PM"
-  ]
-}
-// The above syntax IMPORTS the functions EXPORTED inside "my-paragraph.js"
-function MyToDoApp() {
-  const data = loadTodoListFromServer();
-  const todoList = [];
-  for(let i=0;i<data.length;i++) {
-    todoList.push(<ToDoText text={data[i]} />)
-  }
-  // todoList is a list of all the todo-components
-  return (
-    <div className="container bg-black">
-      {todoList}
-      <br />
-      <br />
-      <ToDoText text={data[0]} />
-      <ToDoText text={data[1]} />
-    </div>
-  )
+function App() {
+  return <div className="container">
+    <UsernameInput />
+  </div>
 }
 
-export default MyToDoApp
+export default App
 // Using the default syntax here, because this file
 // will never have any other function
 // so, I want to clearly specify that this will be the only
