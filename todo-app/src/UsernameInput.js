9a3a2b5c65ddb4849810c53a80d81eac39affb7e
Suhail Gupta <suhailgupta03@gmail.com>
2024-03-08 21:37:20 +0530

    Create UsernameInput.js

---
+++ todo-app/src/UsernameInput.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+
+// <Test name = "john" age=20 />
+// function Test(props) {
+//     const {name, age} = props;
+//     return <div>
+//         <p>{name}</p>
+//         <p>{age}</p>
+//     </div>
+// }
+
+// class based component:
+// component that is rendered using a CLASS
+export class UsernameInput extends React.Component { // UsernameInput is a React.Component
+
+    // React before rendering will create
+    // and object of this class and that is 
+    // when this constructor will be called
+    constructor(props) {
+        // props is a simple OBJECT that holds the properties that 
+        // are passed when rendering the component
+        // <UsernameInput name="foo" />
+        // props : {name: "foo"}
+        super(props); // calling the parent's class constructor
+        // in order for our component to render, the parent component
+        // must also be initalized
+        this.state = {
+            message: "",
+            passwordLength: 0,
+            defaultClassString: "bg-warning rounded mt-3 p-3 fs-5"
+        }
+    }
+
+    myCustomHandler = (event) => {
+        const password = event.target.value;
+        if(password.length < 6) {
+            this.setState({
+                message: "Your password is too short! It needs to be atleast 6 characters",
+                passwordLength: password.length
+            })
+        }else if(!password.includes("@") || !password.includes("#")) {
+            this.setState({
+                message: "Your password must have atleast @ or #",
+                passwordLength: password.length
+            })
+        }else if(!password.match(/\d/g)) {
+            this.setState({
+                message: "Your password must alteast have one digit",
+                passwordLength: password.length
+            })
+        }else {
+            this.setState({
+                message: "Your password is strong enough!",
+                passwordLength: password.length,
+                defaultClassString: "bg-success rounded mt-3 p-3 fs-5"
+            })
+        }
+    }
+
+    // render is a function that is defined in the SUPER
+    // CLASS React.Component which is responsible for
+    // returning the data that will be RENDERED by this
+    // component
+    render() {
+        return <div className="mt-5 d-flex flex-column border rounded p-3 bg-light">
+            <p className="bg-info p-1 fs-5 rounded mb-5">Password Validator</p>
+            <input 
+                type="password"
+                placeholder="Enter your password" 
+                className="fs-3 rounded"
+                onChange={this.myCustomHandler}
+                // when something changes "on input"
+                // I should be notified .. and I will be 
+                // notified by "myCustomHandler"
+            ></input>
+            <div className={this.state.defaultClassString}>
+                <p>
+                    {this.state.message} 
+                </p>
+                <p>
+                    <span>Password Length: {this.state.passwordLength}</span>
+                </p>
+            </div>
+        </div>
+    }
+}
+
