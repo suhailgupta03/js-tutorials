37d980eed76e31e9597be30ebd7b534fad3fbf54
Suhail Gupta <suhailgupta03@gmail.com>
2024-03-11 21:00:26 +0530

    update

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,19 @@
-import {UsernameInput} from "./UsernameInput"
+import { UsernameInput } from "./UsernameInput";
+import { LoadComments } from "./load-comments";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="container">
-    <UsernameInput />
-  </div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/load-comments" Component={LoadComments} />
+        <Route path="/password-validator" Component={UsernameInput} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
 // Using the default syntax here, because this file
 // will never have any other function
 // so, I want to clearly specify that this will be the only
